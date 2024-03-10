afd02179b0e3819b18a3840a95029d39a94a3824
faustine-van <faustinemuhayemariya44@gmail.com>
2024-03-10 23:17:58 +0200

    add redis

---
+++ utils/redis.js
@@ -0,0 +1,58 @@
+// utils/redis.js
+
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    // Display any errors in the console
+    this.client.on('error', (err) => {
+      console.error('Redis Error:', err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+// Create and export an instance of RedisClient called redisClient
+const redisClient = new RedisClient();
+export default redisClient;
