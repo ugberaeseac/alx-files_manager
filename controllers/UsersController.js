a6a9d2e2790fb509c48c87f1b6e0ee048c1d9504
UGBERAESE Charles A <ugberaeseac@gmail.com>
2024-03-11 11:29:22 -0700

    DEBUG: fixed bug on creating a new user with MongoDB

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,24 +1,24 @@
 import sha1 from 'sha1';
-import dbclient from '../utils/db';
+import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(request, response) {
     const { email, password } = request.body;
     if (!email) {
       return response.status(400).send({ error: 'Missing email' });
-    }
+    };
     if (!password) {
       return response.status(400).send({ error: 'Missing password' });
     }
-    const emailExist = await dbclient.usersCollection.find({ email });
+    const emailExist = await dbClient.usersCollection.findOne({ email });
     if (emailExist) {
       return response.status(400).send({ error: 'Already exist' });
     }
 
     const hashedPwd = sha1(password);
-    const insertData = await dbclient.usersCollection.insert({ email, password: hashedPwd });
+    const insertData = await dbClient.usersCollection.insertOne({ email, password: hashedPwd });
 
-    const user = { id: insertData.insertedId, email };
+    const user = { 'id': insertData.insertedId, email };
     return response.status(201).send(user);
   }
 }
