8269f7f79206040ea5f23fcd077e81b0ca5c3377
faustine-van <faustinemuhayemariya44@gmail.com>
2024-03-11 22:07:04 +0200

    added two other routes on task2

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import UsersController from '../controllers/UsersController';
+import AppController from '../controllers/AppController';
 
 function routeController(app) {
   const route = express.Router();
@@ -8,6 +9,8 @@ function routeController(app) {
   route.post('/users', (request, response) => {
     UsersController.postNew(request, response);
   });
+  app.get('/status', AppController.getStatus);
+  app.get('/stats', AppController.getStats);
 }
 
 module.exports = routeController;
